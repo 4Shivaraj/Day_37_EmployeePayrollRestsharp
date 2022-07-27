27cf9daaf77541bc3ecdcc469e74f1d228473c8a
4Shivaraj <4shivaraj.gowda@gmail.com>
2022-07-28 03:53:37 +0530

    [Shivaraj] Added Retrieve all employee details in the json file

---
+++ EmployeePayRoll/EmployeePayrollRestSharp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace EmployeePayrollRestSharp
+{
+    class Program
+    {
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Welcome to Employee Payroll using RestSharp!");
+        }
+    }
+}
+
+/*
+ * UC-1
+Ability to Retrieve all Employees in EmployeePayroll JSON Server
+- Use JSON Server and RESTSharp to save the EmployeePayroll Data of id, name, and salary
+- Retrieve in the MSTest Test and corresponding update the Memory with the Data
+
+ Standard Output: 
+Id: 1	Name: Shivaraj	Salary: 100000
+Id: 2	Name: Cheluvesha	Salary: 98665
+Id: 3	Name: Deepak	Salary: 80000
+Id: 4	Name: Naresh	Salary: 71400
+ */
